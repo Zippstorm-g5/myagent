9ee942ed92ea118452f9e0e41d0acf0d78460393
Zippstorm <29355721+Zippstorm-g5@users.noreply.github.com>
2023-12-03 12:47:29 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,10 +9,11 @@ fi
 # 将提供的参数赋值给变量
 SERVER_URL=$1
 TOKEN=$2
-export SERVER_URL=$SERVER_URL
-export TOKEN=$TOKEN
+
 # 更新包列表并安装所需包
 apt update && apt install wget curl python3 python3-requests python3-netifaces -y
 
 # 下载Python脚本，赋予执行权限并执行
-wget -L -O - https://raw.githubusercontent.com/Zippstorm-g5/myagent/main/main.py | python3
+wget -L -O main.py https://raw.githubusercontent.com/Zippstorm-g5/myagent/main/main.py
+chmod +x main.py
+python3 main.py $SERVER_URL $TOKEN
