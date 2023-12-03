389b391e5d5268d114eaf6fd672adcbe043814e3
Zippstorm <29355721+Zippstorm-g5@users.noreply.github.com>
2023-12-03 11:49:44 +0800

    Update main.sh
    传入URL和Token

--- main.sh
+++ main.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# 检查是否提供了参数
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <token>"
+# 检查是否提供了足够的参数
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <server_url> <token>"
     exit 1
 fi
 
-# 将提供的参数赋值给一个变量
-TOKEN=$1
+# 将提供的参数赋值给变量
+SERVER_URL=$1
+TOKEN=$2
 
 # 更新包列表并安装所需包
 apt update && apt install wget curl python3 python3-requests python3-netifaces -y
@@ -15,4 +16,4 @@ apt update && apt install wget curl python3 python3-requests python3-netifaces -
 # 下载Python脚本，赋予执行权限并执行
 wget -L https://raw.githubusercontent.com/Zippstorm-g5/myagent/main/main.py
 chmod +x main.py
-python3 main.py https://monitor.zippstorm.com/api/v1/server $TOKEN
+python3 main.py $SERVER_URL $TOKEN
